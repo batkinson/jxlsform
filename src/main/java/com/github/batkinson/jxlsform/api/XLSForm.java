27b96c454d52da56b2b020271a01c1ee7047cdc2
Brent Atkinson <brent.atkinson@gmail.com>
2018-07-26 14:58:52 +0100

    Added constants for required and standard xlsform sheet names

--- src/main/java/com/github/batkinson/jxlsform/api/XLSForm.java
+++ src/main/java/com/github/batkinson/jxlsform/api/XLSForm.java
@@ -1,8 +1,15 @@
 package com.github.batkinson.jxlsform.api;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import static java.util.Arrays.asList;
+
 public interface XLSForm extends Iterable<Sheet> {
 
     String SURVEY = "survey", CHOICES = "choices", SETTINGS = "settings";
+    Set<String> REQUIRED_SHEET_NAMES = new LinkedHashSet<>(asList(SURVEY, CHOICES));
+    Set<String> STANDARD_SHEET_NAMES = new LinkedHashSet<>(asList(SURVEY, CHOICES, SETTINGS));
 
     boolean hasSheet(String name);
 
